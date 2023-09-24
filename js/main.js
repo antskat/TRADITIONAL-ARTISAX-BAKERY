39999b868667e1544eaf33b3eac2fb06564bb644
antskat <donaldsmak7@gmail.com>
2023-09-24 20:45:17 +0300

    finish website

--- js/main.js
+++ js/main.js
@@ -33,3 +33,33 @@ order03.addEventListener('click', alarm);
 order03.addEventListener('click', scroll);
 order04.addEventListener('click', alarm);
 order04.addEventListener('click', scroll);
+
+
+
+var popup = document.getElementById('popup');
+var buybtn01 = document.getElementById('buy01');
+var buybtn02 = document.getElementById('buy02');
+var buybtn03 = document.getElementById('buy03');
+var blurBg = document.getElementById('blur-background');
+
+function showPopup() {
+    let elem = document.getElementById('popup');
+    let style = getComputedStyle(elem);
+    if (style.display === 'none') {
+        document.getElementById('popup').style.display='block';
+    } else if (style.display !== 'none') {
+        document.getElementById('popup').style.display='none';
+    }
+
+    if(style.display === 'block') {
+        document.getElementById('blur-background').style.display='block';
+        document.body.style.overflow='hidden';
+    } else if(style.display !== 'block') {
+        document.getElementById('blur-background').style.display='none';
+    }
+}
+
+buybtn.addEventListener('click', showPopup);
+buybtn01.addEventListener('click', showPopup);
+buybtn02.addEventListener('click', showPopup);
+buybtn03.addEventListener('click', showPopup);
