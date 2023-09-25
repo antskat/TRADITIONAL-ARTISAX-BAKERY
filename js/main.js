9c1629d45fc937146150823e501976dbfd38d9b5
antskat <donaldsmak7@gmail.com>
2023-09-25 12:45:54 +0300

    fix some bugs and add close button for popup

--- js/main.js
+++ js/main.js
@@ -1,65 +1,78 @@
-var buybtn = document.getElementById('bybtn');
-var order01 = document.getElementById('order01');
-var order02 = document.getElementById('order02');
-var order03 = document.getElementById('order03');
-var order04 = document.getElementById('order04');
-var scrollObject = document.getElementById('scroll-object');
-var message = document.getElementById('message')
+var buybtn = document.getElementById("bybtn");
+var order01 = document.getElementById("order01");
+var order02 = document.getElementById("order02");
+var order03 = document.getElementById("order03");
+var order04 = document.getElementById("order04");
+var scrollObject = document.getElementById("scroll-object");
+var message = document.getElementById("message");
 
-buybtn.getAttribute('disabled');
+buybtn.getAttribute("disabled");
 
 function scroll() {
-    scrollObject.scrollIntoView({ behavior: "smooth" });
+  scrollObject.scrollIntoView({ behavior: "smooth" });
 }
 
 function alarm() {
-    let elem = document.getElementById('message');
-    let style = getComputedStyle(elem);
-    if (style.display === 'none') {
-        document.getElementById('message').style.display='block';
-    } else if (style.display !== 'none') {
-        document.getElementById('message').style.display='none';
-    }
-    setTimeout(function () {
-        document.getElementById('message').style.display='none';
-    }, 3000);
+  let elem = document.getElementById("message");
+  let style = getComputedStyle(elem);
+  if (style.display === "none") {
+    document.getElementById("message").style.display = "block";
+  } else if (style.display !== "none") {
+    document.getElementById("message").style.display = "none";
+  }
+  setTimeout(function () {
+    document.getElementById("message").style.display = "none";
+  }, 3000);
 }
 
-order01.addEventListener('click', alarm);
-order01.addEventListener('click', scroll);
-order02.addEventListener('click', alarm);
-order02.addEventListener('click', scroll);
-order03.addEventListener('click', alarm);
-order03.addEventListener('click', scroll);
-order04.addEventListener('click', alarm);
-order04.addEventListener('click', scroll);
+order01.addEventListener("click", alarm);
+order01.addEventListener("click", scroll);
+order02.addEventListener("click", alarm);
+order02.addEventListener("click", scroll);
+order03.addEventListener("click", alarm);
+order03.addEventListener("click", scroll);
+order04.addEventListener("click", alarm);
+order04.addEventListener("click", scroll);
 
-
-
-var popup = document.getElementById('popup');
-var buybtn01 = document.getElementById('buy01');
-var buybtn02 = document.getElementById('buy02');
-var buybtn03 = document.getElementById('buy03');
-var blurBg = document.getElementById('blur-background');
+var buybtn01 = document.getElementById("buy01");
+var buybtn02 = document.getElementById("buy02");
+var buybtn03 = document.getElementById("buy03");
+var blurBg = document.getElementById("blur-background");
+var closebtn = document.getElementById("closebtn");
 
 function showPopup() {
-    let elem = document.getElementById('popup');
-    let style = getComputedStyle(elem);
-    if (style.display === 'none') {
-        document.getElementById('popup').style.display='block';
-    } else if (style.display !== 'none') {
-        document.getElementById('popup').style.display='none';
-    }
+  let elem = document.getElementById("popup");
+  let style = getComputedStyle(elem);
+  if (style.display === "none") {
+    document.getElementById("popup").style.display = "block";
+  } else if (style.display !== "none") {
+    document.getElementById("popup").style.display = "none";
+  }
 
-    if(style.display === 'block') {
-        document.getElementById('blur-background').style.display='block';
-        document.body.style.overflow='hidden';
-    } else if(style.display !== 'block') {
-        document.getElementById('blur-background').style.display='none';
-    }
+  if (style.display === "block") {
+    document.getElementById("blur-background").style.display = "block";
+    document.body.style.overflowY = "hidden";
+  } else if (style.display !== "block") {
+    document.getElementById("blur-background").style.display = "none";
+  }
 }
 
-buybtn.addEventListener('click', showPopup);
-buybtn01.addEventListener('click', showPopup);
-buybtn02.addEventListener('click', showPopup);
-buybtn03.addEventListener('click', showPopup);
+function closePopup() {
+  let elem = document.getElementById("popup");
+  let style = getComputedStyle(elem);
+  if (style.display === "block") {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("blur-background").style.display = "none";
+    document.body.style.overflowY = "scroll";
+  } else if (style.display !== "block") {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("blur-background").style.display = "block";
+    document.body.style.overflowY = "hidden";
+  }
+}
+
+buybtn.addEventListener("click", showPopup);
+buybtn01.addEventListener("click", showPopup);
+buybtn02.addEventListener("click", showPopup);
+buybtn03.addEventListener("click", showPopup);
+closebtn.addEventListener("click", closePopup);
